f75f61ea5e09bf4895668491ff9eafb5fa4f1c35
Hussein Khodor <hkhodor03@gmail.com>
2023-08-10 01:08:07 -0700

    Built the follow us block as well as the footer block. Forgot to commit the follow us block initially.

--- javascript/main.js
+++ javascript/main.js
@@ -4,3 +4,35 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+function handleGridDiv() {
+  const gridContainer = document.querySelector('.grid--1x3');
+  const lastFourImages = Array.from(gridContainer.querySelectorAll('.follow-us__image')).slice(-4);
+
+  if (window.innerWidth >= 768) {
+    const existingGridDiv = gridContainer.querySelector('.grid--1x2');
+    if (!existingGridDiv) {
+      const newGridDiv = document.createElement('div');
+      newGridDiv.classList.add('grid', 'grid--1x2');
+
+      lastFourImages.forEach(image => {
+        newGridDiv.appendChild(image);
+      });
+
+      gridContainer.appendChild(newGridDiv);
+    }
+  } else {
+    const existingGridDiv = gridContainer.querySelector('.grid--1x2');
+    if (existingGridDiv) {
+      lastFourImages.forEach(image => {
+        gridContainer.appendChild(image);
+      });
+
+      gridContainer.removeChild(existingGridDiv);
+    }
+  }
+}
+
+handleGridDiv();
+
+window.addEventListener('resize', handleGridDiv);
